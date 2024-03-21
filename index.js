73f86c6fe0a4514b32f0df7bff637189f8114567
IamFazil-Bishar <fazilbishar700@gamil.com>
2024-03-21 06:13:00 +0530

    cors midileware remove

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 import express from "express";
-import dotenv, { config } from "dotenv";
+import dotenv from "dotenv";
 import mongoose from "mongoose";
-import cors from "cors";
 import cookieParser from "cookie-parser";
 
 import tourRoute from './routes/tours.js' 
@@ -10,26 +9,24 @@ import authRoute from './routes/auth.js'
 import reviewRoute from './routes/review.js' 
 import bookingRoute from './routes/booking.js' 
 
-
-
-
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 8000;
 
-// const corsOptions = {
-//   origin: true,
-//   Credential: true
-// }
+// Set up middleware for JSON parsing and cookie parsing
+app.use(express.json());
+app.use(cookieParser());
 
- // Enable CORS middleware
+// Manually set CORS headers
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "https://frontend-iota-ochre-57.vercel.app/");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  next();
+});
 
-app.use(cors({
-  origin: 'https://frontend-iota-ochre-57.vercel.app/',
-  credentials: true // Allow credentials to be sent
-}));
-
-// database connection
+// Connect to MongoDB
 mongoose.set("strictQuery", false);
 const connect = async () => {
   try {
@@ -37,24 +34,21 @@ const connect = async () => {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
-    console.log("MongoDB database Conntected");
+    console.log("MongoDB database Connected");
   } catch (err) {
-    console.log("MongoDB database Connectoion failed", err);
+    console.log("MongoDB database Connection failed", err);
   }
 };
 
-// middleware
-app.use(express.json());
-app.use(cors(corsOptions));
-app.use(cookieParser());
+// Routes
 app.use("/api/v1/auth", authRoute);
 app.use("/api/v1/tours", tourRoute);
 app.use("/api/v1/users", userRoute);
 app.use("/api/v1/review", reviewRoute);
 app.use("/api/v1/booking", bookingRoute);
 
-
+// Start server
 app.listen(port, () => {
   connect();
-  console.log("server listening on port", port);
+  console.log("Server listening on port", port);
 });
