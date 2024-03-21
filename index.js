f196384f8c524121c43f23333ce6f8c35d3ab018
IamFazil-Bishar <fazilbishar700@gamil.com>
2024-03-21 06:43:48 +0530

    cors

--- index.js
+++ index.js
@@ -18,14 +18,21 @@ app.use(express.json());
 app.use(cookieParser());
 
 // Manually set CORS headers
+// Manually set CORS headers
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "https://frontend-iota-ochre-57.vercel.app");
+  const allowedOrigins = ["https://frontend-iota-ochre-57.vercel.app"];
+  const origin = req.headers.origin;
 
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader("Access-Control-Allow-Origin", origin);
+  }
+  
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   res.setHeader("Access-Control-Allow-Credentials", "true");
   next();
 });
+
 
 // Connect to MongoDB
 mongoose.set("strictQuery", false);
