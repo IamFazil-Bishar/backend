86cc79dbff207e9ab8760169eb62e0de030baad7
IamFazil-Bishar <fazilbishar700@gamil.com>
2024-03-21 06:25:12 +0530

    m

--- index.js
+++ index.js
@@ -19,7 +19,8 @@ app.use(cookieParser());
 
 // Manually set CORS headers
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "https://frontend-iota-ochre-57.vercel.app/");
+  res.setHeader("Access-Control-Allow-Origin", "https://frontend-iota-ochre-57.vercel.app");
+
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   res.setHeader("Access-Control-Allow-Credentials", "true");
