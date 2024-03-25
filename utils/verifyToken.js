1e6a0b25fc39a4a4e95c7ef958b4f60951e4faa4
IamFazil-Bishar <fazilbishar700@gamil.com>
2024-03-25 06:00:12 +0530

    debugg

--- utils/verifyToken.js
+++ utils/verifyToken.js
@@ -3,7 +3,7 @@ import jwt from 'jsonwebtoken';
 export const verifyToken = (req, res, next) => {
     const token = req.cookies.accessToken;
 
-    console.log('token:',token)
+    console.log('token:', token); // Log the token for debugging
 
     if (!token) {
         return res.status(401).json({ success: false, message: "You are not authorized" });
@@ -12,6 +12,7 @@ export const verifyToken = (req, res, next) => {
     // if the token exists, then verify the token
     jwt.verify(token, process.env.JWT_SECRET_KEY, (err, user) => {
         if (err) {
+            console.error('JWT verification error:', err); // Log JWT verification error
             return res.status(401).json({ success: false, message: "Token is invalid" });
         }
         req.user = user;
@@ -21,6 +22,7 @@ export const verifyToken = (req, res, next) => {
 
 export const verifyUser = (req, res, next) => {
     verifyToken(req, res, () => {
+        console.log('User:', req.user); // Log the user object for debugging
         if (req.user.id === req.params.id || req.user.role === 'admin') {
             next();
         } else {
@@ -31,6 +33,7 @@ export const verifyUser = (req, res, next) => {
 
 export const verifyAdmin = (req, res, next) => {
     verifyToken(req, res, () => {
+        console.log('User:', req.user); // Log the user object for debugging
         if (req.user.role === 'admin') {
             next();
         } else {
